f5e5b0f8782d0a178af82b80b5849c7a84123ef8
Varun Sahni <84265414+varun-sahni@users.noreply.github.com>
2026-07-13 11:50:03 +0530

    Add: is-palindrome - submission-0

---
+++ Data Structures & Algorithms/is-palindrome/submission-0.py
@@ -0,0 +1,7 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+      newStr = ''
+      for i in s:
+        if i.isalnum():
+            newStr += i.lower()
+      return newStr == newStr[::-1]
